f0c13a3af5e96cdf06dbcaa1988756c659beaa98
Brian De Maio <bdemaio1@gmail.com>
2016-09-12 19:55:28 -0400

    adding usernames and planned p2p private chat

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,30 @@
-var app=angular.module('chatApp', ['ui.bootstrap'])
+var app=angular.module('chatApp', ['ui.bootstrap','ui.router'])
+        .config(function($stateProvider,$urlRouterProvider) {
+          $urlRouterProvider.otherwise("/login");
+          $stateProvider
+            .state('chat',{
+              url:'/chat',
+              templateUrl:'chat.html',
+              controller:'ChatCtrl'
+            })
+              .state('login',{
+                  url:'/login',
+                  templateUrl:'login.html',
+                  controller:'ChatCtrl'
+              })
+        });
 
 app.factory('socket', function(){
   var socket=io.connect();
   return socket;
-})
+});
 
-app.controller('ChatCtrl', function($scope,socket,$http,$log)
+app.controller('ChatCtrl', function($scope,socket,$http,$log,$state)
 {
   $scope.msgs=[];
 
   $scope.$watch('msg',function(){
-    //get response for data based on artist name from spotify api
+    //get response for data based input and output language
     $http({
       method:'GET',
       url:'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160723T144020Z.0c10deb189f9465d.aad68393900352c2aa9b1632bcacb766fbd107f8&text='+$scope.msg+'&lang=en-es'})
@@ -23,22 +37,32 @@ app.controller('ChatCtrl', function($scope,socket,$http,$log)
       });
     });
 
-  $scope.sendMsg = function()
-  {
-    if(!$scope.msg)
-    {return;}
+  $scope.sendMsg = function() {
+      if (!$scope.msg) {
+          return;
+      }
 
-    $scope.msg=$scope.output.text;
-    socket.emit('send msg', $scope.msg); 
-    $scope.msg='';
+      $scope.msg = $scope.output.text;
+      socket.emit('send msg', $scope.msg);
+      $scope.msg = '';
+  }
+
+  $scope.sendUser=function(){
+      var user=$scope.user;
+    socket.emit('new user',user);
+    $state.go('chat');
   }
 
   socket.on('get msg', function(msg)
   {
     $scope.msgs.push(msg);
     $scope.$digest();
-  })
-})
+  });
+
+    socket.on('get users', function(data){
+        $scope.usernames=data;
+    });
+});
 
 
 
--- server.js
+++ server.js
@@ -2,20 +2,53 @@ var express = require('express');
 var app=express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+users=[];
+connections=[];
+
+//added mongodb client, looking to put in mongoose module in future to save messages for users
+//var MongoClient = require('mongodb').MongoClient;
+//var assert = require('assert');
+//var url = 'mongodb://localhost:27017/chat';
+//end of mongo client addition
 
 console.log("We're in server");
 
 app.use(express.static(__dirname+'/public'));
 app.use(express.static(__dirname+'/node_modules'));
 
-io.on('connection', function(socket){
-  socket.on('send msg', function(data){
-    io.emit('get msg', data);
-    console.log(data);
-  });
+//socket connections
+io.on('connection',function(socket) {
+	connections.push(socket);
+
+	console.log('Connected: %s sockets connected', connections.length);
+
+	socket.on('disconnect',function(data){
+		if(!socket.username)return;
+		users.splice(users.indexOf(socket.username),1);
+		updateUsernames();
+		connections.splice(connections.indexOf(socket),1);
+		console.log('Disconnected: %s sockets connected', connections.length);
+	});
+
+	socket.on('send msg', function (data) {
+		io.emit('get msg', data);
+		console.log(data);
+	});
+
+	socket.on('new user', function(data){
+		socket.username=data;
+		users.push(socket.username);
+		console.log(socket.username);
+		console.log(users);
+		updateUsernames();
+	});
+
+	function updateUsernames(){
+		io.sockets.emit('get users',users);
+	}
 });
 
 http.listen(process.env.PORT || 5000);
-
+//chat connection end
 
 
