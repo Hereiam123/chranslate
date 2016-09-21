f7a90b6ca71bdae3ea4448c14e58b61d2a6672d7
Brian De Maio <bdemaio1@gmail.com>
2016-09-21 03:53:03 -0400

    problems

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -71,7 +71,7 @@ app.factory('auth', ['$http','$window','$state','socket',function($http,$window,
     };
     auth.logOut=function(){
         $window.localStorage.removeItem('chranslate-token');
-        socket.disconnect();
+        $state.go('login');
     };
     return auth;
 }]);
@@ -115,9 +115,6 @@ app.controller('AuthCtrl', ['$scope','$state','auth', function($scope,$state,aut
         }).then(function(){
             $state.go('chat');
         });
-    };
-    $scope.logOut=function(socket){
-
     };
 }]);
 
