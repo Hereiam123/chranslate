1c150687f951655240fbb6bd25e41f60898482e4
Brian De Maio <bdemaio1@gmail.com>
2016-09-21 11:38:20 -0400

    string split

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -282,8 +282,8 @@ app.controller('ChatCtrl', ['$scope','socket','$http','$log','setLanguage','auth
 
     socket.on('load old msgs', function(data){
         var messages=$window.localStorage['chat-messages'];
-        messages=messages.split(',');
-        console.log(messages);
+        var res=messages.split(',');
+        console.log(res);
         //$scope.msgs=JSON.parse(messages);
         for(i=0; i<=data.length-1; i++) {
             $scope.msgs.push({user:data[i].username, msg:data[i].msg, date:data[i].created});
